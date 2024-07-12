a081816bf73521aa9a73a718bbf5557c7a00a96c
Azariah Keith <yugenzariah@gmail.com>
2024-07-12 16:09:29 +1200

    Added smooth scrolling functionality

--- public/js/home.js
+++ public/js/home.js
@@ -1,5 +1,5 @@
 // Upcoming events "Show more" button
-document.getElementById('toggle-events').addEventListener('click', function() {
+document.getElementById('toggle-events').addEventListener('click', function () {
     let container = document.getElementById('events-container');
     if (container.style.maxHeight) {
         container.style.maxHeight = null;
@@ -8,4 +8,13 @@ document.getElementById('toggle-events').addEventListener('click', function() {
         container.style.maxHeight = container.scrollHeight + 'px';
         this.textContent = 'Show Less';
     }
+});
+
+// Smooth scrolling for home navigation
+document.getElementById('home-link').addEventListener('click', function (event) {
+    event.preventDefault();
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
 });
